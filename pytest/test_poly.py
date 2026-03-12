e5094cda209271e8e494075a0e33ec95626a2669
Valentinfrm <valentinfromm@proton.me>
2026-03-12 21:23:13 +0100

    fix mul test and add acvp capsulation tests

--- pytest/test_poly.py
+++ pytest/test_poly.py
@@ -1,21 +1,20 @@
-from polynomial import poly
-"""change q = 41 in params
-def test_poly_mul():
-    p1 = poly([23, 0, 11, 7])
-    p2 = poly([40, 5, 16, 0])
-    result = p1.poly_mul(p2)
-    assert result.coeff == [12, 3, 29, 7]
-"""
+from ntt import NTT, iNTT, mul_NTT
 
 def test_poly_mul_kyber():
     from kyber_py.polynomials.polynomials_generic import GenericPolynomialRing
     R = GenericPolynomialRing(3329, 256)
     coeffs1 = list(range(256))
     coeffs2 = list(range(256, 512))
+    
+    # Regular multiplication in kyber_py
     f = R(coeffs1)
     g = R(coeffs2)
-    expected = list((f * g).coeffs)
-    p1 = poly(coeffs1)
-    p2 = poly(coeffs2)
-    result = p1 * p2
-    assert result.coeff == expected
+    regular_product = f * g
+    
+    # NTT
+    ntt_f = NTT(coeffs1)
+    ntt_g = NTT(coeffs2)
+    ntt_product = mul_NTT(ntt_f, ntt_g)
+    your_result = iNTT(ntt_product)
+    
+    assert list(your_result) == list(regular_product.coeffs)
